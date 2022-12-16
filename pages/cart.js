3df0b06d87f88b2bee7fc4ed1694669e3ff25ec4
rbeazley18 <robertbeazley18@gmail.com>
2022-12-16 12:27:18 -0500

    setup

---
+++ pages/cart.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function FirstPost() {
+    return (
+        <>
+            <Head>
+                <title>Cart</title>
+            </Head>
+            <h1>Shopping Cart</h1>
+        </>
+    );
+}
