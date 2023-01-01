083f86c29b74a5a5f6a8f0111826601204f3873f
rbeazley18 <robertbeazley18@gmail.com>
2023-01-01 13:58:40 -0500

    working on weather widget

--- components/weather.js
+++ components/weather.js
@@ -3,28 +3,27 @@ import { loadWeather } from "../lib/openweather";
 export default function WeatherWidget({ weatherData }) {
     // console.log(weatherData);
 
-    // if (weatherData) {
+    if (weatherData) {
     return (
-        <div>{weatherData}</div>
-        // weatherData.map((weather) => (
-        //     <>
-        //         <div className="card text-center col-md-8 mx-auto" key={weather.lat}>
-        //             <div className="card-header">
-        //                 Weather
-        //             </div>
-        //             <div className="card-body">
-        //                 <h5 className="card-title">{weather.current}</h5>
-        //                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        //                 <a href="#" className="btn btn-primary">Expand Weather</a>
-        //             </div>
-        //             <div className="card-footer text-muted">
-        //                 2 days ago
-        //             </div>
-        //         </div>
-        //     </>
-        // ))
+        weatherData.map((weather) => (
+            <>
+                <div className="card text-center col-md-8 mx-auto" key={weather.lat}>
+                    <div className="card-header">
+                        Weather
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{weather.current}</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" className="btn btn-primary">Expand Weather</a>
+                    </div>
+                    <div className="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+            </>
+        ))
     )
-    // }
+    }
 }
 
 
