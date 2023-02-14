5976ccc8bbc087c8544534678feb269248a2a16a
rbeazley18 <robertbeazley18@gmail.com>
2023-02-14 16:35:58 -0500

    remove gh pages script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: '/gh-pages-test',
+    // basePath: '/gh-pages-test',
 }
 
 module.exports = nextConfig
