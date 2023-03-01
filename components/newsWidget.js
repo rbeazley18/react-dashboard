eff9d65080cbe7ad0a534b48e76a1ad29056c0ef
rbeazley18 <robertbeazley18@gmail.com>
2023-03-01 14:51:27 -0500

    redoing news api call

--- components/newsWidget.js
+++ components/newsWidget.js
@@ -26,36 +26,36 @@ export default function NewsWidget() {
     }, [newsData])
 
     const handleSubmit = async (event) => {
-        // Stop the form from submitting and refreshing the page.
+    //     // Stop the form from submitting and refreshing the page.
         event.preventDefault()
-        // Get data from the form.
-        const data = {
-            category: item
-        }
-        // Send the data to the server in JSON format.
-        const JSONdata = JSON.stringify(data);
-        // console.log(JSONdata);
-        // API endpoint where we send form data.
-        const endpoint = '/api/addToCartForm'
-        // Form the request for sending data to the server.
-        const options = {
-            // The method is POST because we are sending data.
-            method: 'POST',
-            // Tell the server we're sending JSON.
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // Body of the request is the JSON data we created above.
-            body: JSONdata,
-        }
-        // Send the form data to our forms API on Vercel and get a response.
-        const response = await fetch(endpoint, options)
+    //     // Get data from the form.
+    //     const data = {
+    //         category: item
+    //     }
+    //     // Send the data to the server in JSON format.
+    //     const JSONdata = JSON.stringify(data);
+    //     // console.log(JSONdata);
+    //     // API endpoint where we send form data.
+    //     const endpoint = '/api/addToCartForm'
+    //     // Form the request for sending data to the server.
+    //     const options = {
+    //         // The method is POST because we are sending data.
+    //         method: 'POST',
+    //         // Tell the server we're sending JSON.
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         // Body of the request is the JSON data we created above.
+    //         body: JSONdata,
+    //     }
+    //     // Send the form data to our forms API on Vercel and get a response.
+    //     const response = await fetch(endpoint, options)
 
-        // Get the response data from server as JSON.
-        // If server returns the name submitted, that means the form works.
-        const result = await response.json()
-        // alert(`Your item: ${JSON.stringify(result.data)}`);
-        // console.log(`Result: ${result}`);
+    //     // Get the response data from server as JSON.
+    //     // If server returns the name submitted, that means the form works.
+    //     const result = await response.json()
+    //     // alert(`Your item: ${JSON.stringify(result.data)}`);
+    //     // console.log(`Result: ${result}`);
     }
 
     if (newsSwitchStatus) {
